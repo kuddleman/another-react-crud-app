da087c8dcc39f3b6897b1f19b7f61a0a460b3749
Kuddleman <edl1155@gmail.com>
2020-06-14 07:21:24 -0700

    complete get products function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
 const products = [
   {
+    id: 1,
     name: 'iPad',
     price: 200
   },
   {
+    id: 2,
     name: 'iPhone',
     price: 650
   }
@@ -15,12 +17,45 @@ const products = [
 
 localStorage.setItem('products', JSON.stringify(products))
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Products Manager</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props) 
+      this.state = {
+        products: []
+      }
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = () => {
+    const products = JSON.parse(localStorage.getItem('products'))
+    console.log('Here are the products:', products)
+    this.setState( { products } )
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+        {
+          this.state.products.map( product => {
+              return (
+                <div key={ product.id }>
+                  <span>{ product.name }</span>
+                   {` \ `}
+                   <span>{ product.price }</span>
+                   {` | `}
+                </div>
+              )
+          } )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
